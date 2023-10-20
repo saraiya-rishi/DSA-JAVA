7280576136813fc8ef36d339d0f40ea1293227f3
saraiya-rishi <saraiyarishi8102@gmail.com>
2023-10-20 23:13:07 +0530

    QUEUE

--- mca/Queue.java
+++ mca/Queue.java
@@ -1,12 +1,11 @@
-
 package com.mycompany.mca;
 
 import java.util.Scanner;
 
 public class Queue {
-    
+
     int q[];
-    int front,rear;
+    int front, rear;
     int size;
 
     public Queue(int size) {
@@ -15,20 +14,97 @@ public class Queue {
         this.rear = -1;
         this.size = size;
     }
+
+    boolean isEmpty() {
+        return front == -1;
+    }
+
+    boolean isFull() {
+        return rear == size - 1;
+    }
+
+    void enQueue(int value) 
+    {
+        if(isFull())
+        {
+            System.out.println("Queue is Full...");
+        }
+        else if(isEmpty())
+        {
+            front = rear = 0;
+            q[rear] = value;
+        }
+        else
+        {
+            rear++;
+            q[rear] = value;
+        }
+    }
     
+    void display()
+    {
+        if(isEmpty())
+        {
+            System.out.println("Queue is Empty...");
+        }
+        else
+        {
+            for(int i=front;i<=rear;i++)
+            {
+                System.out.println(q[i]);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        
-        System.out.print("Enter Size Of Queue:- "); 
-        int n = s.nextInt();
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Size Of Queue:- ");
+        int n = sc.nextInt();
+
         Queue q = new Queue(n);
         
+        boolean key = true;
         
-        
-        
-     }
-    
-    
-   
+        while(key)
+        {
+            System.out.println("1. Insert The Data");
+            System.out.println("2. Delete The Data");
+            System.out.println("3. Peek Of The Data");
+            System.out.println("4. Display The Data");
+            System.out.println("5. Exit...");
+            
+            System.out.print("Enter Your Choice:-");
+            int choice= sc.nextInt();
+            
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter Value:- ");
+                    int value = sc.nextInt();
+                    
+                    q.enQueue(value);
+                    break;
+                    
+                case 2:
+                    break;
+                    
+                case 3:
+                    break;
+                    
+                case 4:
+                    q.display();
+                    break;
+                    
+                case 5:
+                    key = false;
+                    break;
+                    
+             
+                default:
+                    System.out.println("Please Enter Right Choice...");
+            }
+        }
+
+    }
+
 }
